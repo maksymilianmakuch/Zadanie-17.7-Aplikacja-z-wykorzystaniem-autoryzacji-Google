74740cb31d23fb6706631ac62684c7c3f918cf39
Maksym <maksymilian.makuch7@gmail.com>
2018-01-04 20:22:03 +0100

    Działa

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID':'183835007079-q5uf20p6jphrs65qe4dlggkepshul1ql.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET':'S9iqCXo6Z0GpWmp75uzAHRot',
+	'CALLBACK_URL':'http://localhost:3000/auth/google/callback'
+};
